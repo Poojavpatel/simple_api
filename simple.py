7b46faada90bac694442d17764be1d5e1e52d0fc
Poojavpatel <32796407+Poojavpatel@users.noreply.github.com>
2019-03-21 22:40:01 +0530

    adding cors to allow cross origin requests|sending response as json

--- simple.py
+++ simple.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
 import random
+from flask_responses import json_response
 
 app=Flask(__name__)
 api= Api(app)
+CORS(app)
 
 resultlist = ["Fake","Real"]
 
@@ -12,7 +15,7 @@ class User(Resource):
         # randomly choosing real or fake and return with response status 200
         result = random.choice(resultlist)
         print(result)
-        return result, 200
+        return json_response({"result":result}, status_code=200)
     
     def post(self, name):
         a = "url added to db"
