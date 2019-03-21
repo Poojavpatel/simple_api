4611d305776408b38fd7ce855fae55506b823779
Poojavpatel <32796407+Poojavpatel@users.noreply.github.com>
2019-03-21 14:13:37 +0530

    made a flask api that runs perfectly locally

---
+++ simple.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import random
+
+app=Flask(__name__)
+api= Api(app)
+
+resultlist = ["Fake","Real"]
+
+class User(Resource):
+    def get(self):
+        # randomly choosing real or fake and return with response status 200
+        result = random.choice(resultlist)
+        print(result)
+        return result, 200
+    
+    def post(self, name):
+        a = "url added to db"
+        return a,200
+
+api.add_resource(User, "/")
+app.run(debug=True)
